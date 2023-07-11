5e0f41c8a1b850df618768d36e6330d2b3a503d7
Igor <igorrafael_9@hotmail.com>
2023-07-11 17:18:36 -0300

    Listagem e cadastro de usuários

---
+++ ControleDeContatos/Controllers/UsuarioController.cs
@@ -0,0 +1,47 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Index()
+        {
+            List<UsuarioModel> usuarios = _usuarioRepositorio.BuscaTodos();
+
+            return View(usuarios);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuario)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _usuarioRepositorio.Adicionar(usuario);
+                    TempData["MensagemSucesso"] = "Usuario salvo com sucesso";
+                    return RedirectToAction("Index");
+                }
+
+                return View(usuario);
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu usuario, tente novamente mais tarde, detalhe do erro: {ex.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
--- ControleDeContatos/Program.cs
+++ ControleDeContatos/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
 
 var app = builder.Build();
 
